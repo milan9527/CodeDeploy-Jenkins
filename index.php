5896a13330cb3f47d81368e3cc07ba14f0ab90e4
milan9527 <milan9527@hotmail.com>
2018-10-28 15:51:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 </head>
 <body>
   <div align="center">
-    <h1>AWS DevOps Demo - V3</h1>
+    <h1>AWS DevOps Demo - V1</h1>
     <h2>Github + CodeDeploy + Jenkins</h2> 
     <h2>AWS BJS CodeDeploy Demo</h2>
   </div>
